3490860475884da2014a4d10d37a8031875668ae
ChinPangLung <chinpang9527@gmail.com>
2018-07-26 10:41:03 +0800

    stream

--- stream-hello/src/main/java/com/example/streamhello/SinkReceiver.java
+++ stream-hello/src/main/java/com/example/streamhello/SinkReceiver.java
@@ -13,12 +13,23 @@ import org.springframework.cloud.stream.messaging.Sink;
  * @version: V1.0
  * @Description: Created with IntelliJ IDEA.
  **/
+
+/**
+ * 该注解用来指定一个或者多个定义了@Input或者@Output注解的接口，以实现对消息通道的绑定。
+ *
+ * @EnableBinding(Sink.class)表示绑定了Sink接口，该接口是spring cloud stream中默认实现的对输入消息通道绑定的定义
+ */
 @EnableBinding(Sink.class)
 public class SinkReceiver {
     private static Logger logger = LoggerFactory.getLogger(StreamHelloApplication.class);
 
+    /**
+     * 把receiver方法注册为input消息通道的事件监听器
+     *
+     * @param payload
+     */
     @StreamListener(Sink.INPUT)
     public void receiver(Object payload) {
-        logger.info("Receiver: " + payload);
+        logger.info("Receiver: " + payload.toString());
     }
 }
