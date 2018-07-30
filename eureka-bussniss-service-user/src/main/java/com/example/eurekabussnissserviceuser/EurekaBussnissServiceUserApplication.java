b5ca12f4c464b516003dfd06e56ddeb3c1cfebf7
ChinPangLung <chinpang9527@gmail.com>
2018-07-30 16:04:27 +0800

    update

--- eureka-bussniss-service-user/src/main/java/com/example/eurekabussnissserviceuser/EurekaBussnissServiceUserApplication.java
+++ eureka-bussniss-service-user/src/main/java/com/example/eurekabussnissserviceuser/EurekaBussnissServiceUserApplication.java
@@ -9,9 +9,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class EurekaBussnissServiceUserApplication {
 
     public static void main(String[] args) {
-        System.out.println("===========================================");
+//        System.out.println("===========================================");
         /**
          * Springboot整合Elasticsearch 在项目启动前设置一下的属性，防止报错
+         * 解决netty冲突后初始化client时还会抛出异常
          * java.lang.IllegalStateException: availableProcessors is already set to [4], rejecting [4]
          */
         System.setProperty("es.set.netty.runtime.available.processors", "false");
