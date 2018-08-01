fbc9e8aa672c61cbd4b7f8b32476f0a98b5e3321
ChinPangLung <chinpang9527@gmail.com>
2018-08-01 16:22:41 +0800

    update

--- eureka-zuul/src/main/java/com/example/eurekazuul/EurekaZuulApplication.java
+++ eureka-zuul/src/main/java/com/example/eurekazuul/EurekaZuulApplication.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Bean;
 @EnableZuulProxy//注解@EnableZuulProxy以开启zuul特性
 @SpringCloudApplication //使用@SpringCloudApplication包括，
 // Springboot注解，注册服务到注册中心注解，熔断器注解
-// 在SpringCloud看来这是每一微服务所必须应有的三个注解，所以才推出了@SpringCloudApplication注解
+// 在SpringCloud看来这是每一微服务所必须应有的三个注解。这里所以才推出了@SpringCloudApplication注解
 public class EurekaZuulApplication {
 
     public static void main(String[] args) {
